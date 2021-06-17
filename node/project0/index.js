3c800fdb6c6610a8a9ab82ade0d3be0e088d08c7
tai <minhtaile2712@gmail.com>
2021-06-17 20:10:11 +0700

    hh

--- node/project0/index.js
+++ node/project0/index.js
@@ -1,15 +1,59 @@
-console.log("program started");
+console.log("windows");
+const bytes1 = 500105249280;
+printSize(bytes1);
+printSize(476939 * 1024 * 1024);
 
-setImmediate(() => console.log("from immediate 1"));
+console.log("fedora");
+const size2 = 500107862016;
+printSize(size2);
+const p21 = 104857600;
+printSize(p21);
+const p22 = 16777216;
+printSize(p22);
+const p23 = 155853052928;
+printSize(p23);
+const p24 = 532676608;
+printSize(p24);
+const p2x = 343598456832;
+printSize(p2x);
+console.log("wasted");
+printSize(size2 - p21 - p22 - p23 - p24 - p2x);
 
-console.log("program progress 1");
+console.log(
+  "((976773168-320*1024*1024*2)-305682432)*512/1024 is ",
+  ((976773168 - 320 * 1024 * 1024 * 2 - 305682432) * 512) / 1024
+);
 
-console.log("program progress 2");
+function printSize(bytes) {
+  const divisor = 1024;
+  const result = [];
+  let dividend = bytes;
+  while (dividend != 0) {
+    remainder = dividend % divisor;
+    dividend = Math.floor(dividend / divisor); //quotient
+    result.push(remainder);
+  }
 
-setImmediate(() => console.log("from immediate 2"));
+  let str = "";
+  for (var i = 0; i < result.length; i++) {
+    if (result[i]) str = result[i] + sizename(i) + " " + str;
+  }
+  console.log("Size is", str);
+}
 
-setTimeout(() => console.log("from timeout"), 0);
-
-console.log("end of program");
-
-process.nextTick(console.log, "from nextTick");
+function sizename(level) {
+  switch (level) {
+    case 0:
+      return "bytes";
+    case 1:
+      return "KiB";
+    case 2:
+      return "MiB";
+    case 3:
+      return "GiB";
+    case 4:
+      return "TiB";
+    default:
+      return "xB";
+  }
+}
