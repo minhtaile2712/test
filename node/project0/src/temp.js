235dc3110df7b35c81b080306bed984ba1158d22
tai <minhtaile2712@gmail.com>
2021-06-18 16:24:46 +0700

    hi

--- node/project0/src/temp.js
+++ node/project0/src/temp.js
@@ -1,6 +1,7 @@
-XLSX = require("xlsx");
-// var workbook = XLSX.readFile("../Book.xlsx");
-var workbook = XLSX.readFile("../Sheet1x.csv");
+const XLSX = require("xlsx");
+const decode_range = XLSX.utils.decode_range;
+const encode_cell = XLSX.utils.encode_cell;
+
 console.log("workbook", workbook);
 
 var address_of_cell = "A2";
@@ -11,3 +12,28 @@ console.log("worksheet", worksheet);
 var desired_cell = worksheet[address_of_cell];
 var desired_value = desired_cell ? desired_cell.v : undefined;
 console.log(address_of_cell, desired_value);
+
+try {
+  const workbook = XLSX.readFile("./uploads/sheet1.csv");
+  // console.log("workbook", workbook);
+  const firtSheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[firtSheetName];
+  console.log("worksheet", worksheet);
+
+  const cellRange = decode_range(worksheet["!ref"]);
+  for (var R = cellRange.s.r; R <= cellRange.e.r; ++R) {
+    for (var C = cellRange.s.c; C <= cellRange.e.c; ++C) {
+      const cellAddress = { c: C, r: R };
+      const cellAddressA1 = encode_cell(cellAddress);
+
+      const workcell = worksheet[cellAddressA1];
+      const workcellValue = workcell ? workcell.v : undefined;
+      console.log(cellAddressA1, workcellValue);
+    }
+  }
+
+  return res.send("ok");
+} catch (error) {
+  console.log(error.message);
+  return res.sendStatus(500);
+}
